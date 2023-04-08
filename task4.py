5cad54ef0a193f8f7ca5c4f4e2965f102d416bb9
Teona Gogichashvili <teonafreckling@gmail.com>
2023-04-08 20:12:23 +0300

    4th task

---
+++ task4.py
@@ -0,0 +1,4 @@
+seryozhas_cranes = int(input("how many cranes made Seryozha? "))
+petyas_cranes = seryozhas_cranes
+katyas_cranes = (seryozhas_cranes+petyas_cranes)*2
+print(seryozhas_cranes, petyas_cranes, katyas_cranes)
